12a11bfe58880a048f1c9e791614c23426d81a53
SafdarAliGit <safdar211@gmail.com>
2026-01-11 22:56:37 +0500

    updatedae

--- fbr/api/__init__.py
+++ fbr/api/__init__.py
@@ -7,7 +7,7 @@ class FBRDigitalInvoicingAPI:
     def __init__(self,settings):
         self.settings = settings
         self.base_url = settings.get("url")
-        self.token = frappe.utils.password.get_decrypted_password(settings.get("token"))
+        self.token = frappe.utils.password.get_decrypted_password("Fbr Settings Item", settings.get("name"), fieldname="token")
        
     def init_request(self):
         self.headers = {
