6652d5a3d01a2104c5fd559ad130fdecc0de23c3
SafdarAliGit <safdar211@gmail.com>
2026-01-11 14:29:39 +0500

    updzsted

--- fbr/api/__init__.py
+++ fbr/api/__init__.py
@@ -20,7 +20,7 @@ class FBRDigitalInvoicingAPI:
 
     def make_request(self, method, endpint, data=None):
         self.init_request()
-        request = self.session.request(method, f"{self.base_url}/{endpint}", json=data)
+        request = self.session.request(method, f"https://gw.fbr.gov.pk/{endpint}", json=data)
         if request.status_code != 200:
             
             frappe.log_error(
@@ -30,19 +30,3 @@ class FBRDigitalInvoicingAPI:
             frappe.throw(f"Error in FBR Invoicing API: {request.text}")
         return request.json()
 
-    @property
-    def get_settings_item(self):
-        """Get single row by company_tax_id"""
-        try:
-            row = frappe.db.get_value(
-                "Fbr Settings Item",
-                {"company_tax_id": self.company_tax_id},
-                ["*"],
-                as_dict=True
-            )
-            return row
-        except frappe.DoesNotExistError:
-            return None
-        except Exception as e:
-            frappe.log_error(f"Error fetching FBR item: {str(e)}")
-            return None 
